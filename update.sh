6ea56b2ea3ef9123a243638036a4ed1020154208
Michael Ripley <zkxs00@gmail.com>
2016-02-02 19:13:06 -0600

    Set up system for hiding sensitive information.

---
+++ update.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+cp -a Compat.ini realconfig/
+cp -a DeviceProfiles.ini realconfig/
+cp -a Engine.ini realconfig/
+cp -a Game.ini realconfig/
+cp -a Input.ini realconfig/
+cp -a Lightmass.ini realconfig/
+cp -a Scalability.ini realconfig/
+
+ADMINPASS=$(cat ServerAdminPassword.secret)
+PASS=$(cat ServerPassword.secret)
+PORT=$(cat RCONPort.secret)
+
+cat GameUserSettings.ini | sed \
+	-e "s/ServerAdminPassword=.*/ServerAdminPassword=$ADMINPASS/" \
+	-e "s/ServerPassword=.*/ServerPassword=$PASS/" \
+	-e "s/RCONPort=.*/RCONPort=$PORT/" \
+	> realconfig/GameUserSettings.ini
+
