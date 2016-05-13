6c4bee0a819fa127aeda57723edef553067de4c1
Michael Ripley <zkxs00@gmail.com>
2016-05-13 14:13:43 -0500

    Made printserverchanges.sh also print Game.ini

--- printserverchanges.sh
+++ printserverchanges.sh
@@ -1,2 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+echo "GameUserSettings.ini:"
 diff --suppress-common-lines -y GameUserSettings.ini realconfig/GameUserSettings.ini | grep -v 'CENSORED'
+echo -e "\nGame.ini:"
+diff --suppress-common-lines -y Game.ini realconfig/Game.ini
+echo -ne "\n"
