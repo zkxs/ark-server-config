ed1c2778317dbd7be5006e143a55848ea119f6f2
Atppirate <captainatpbeard@gmail.com>
2017-06-06 14:18:48 -0500

    Increasing Difficulty
    Max Dino will now be 210
    Loot is scaled to 700%

--- ds.sh
+++ ds.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 umask 002
-./ark/ShooterGame/Binaries/Linux/ShooterGameServer TheCenter?listen?SessionName=DannoDanDan?MaxPlayers=30?BanListURL="http://playark.com/banlist.txt"?AlwaysNotifyPlayerJoined=True?AlwaysNotifyPlayerJoin=True -NoBattlEye -nosteamclient -server -log
+./ark/ShooterGame/Binaries/Linux/ShooterGameServer TheCenter?listen?SessionName=DannoDanDan?MaxPlayers=30?BanListURL="http://playark.com/banlist.txt"?AlwaysNotifyPlayerJoined=True?AlwaysNotifyPlayerJoin=True?OverrideOfficialDifficulty=7.0 -NoBattlEye -nosteamclient -server -log
