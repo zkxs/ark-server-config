546a92553370e8bcae3061ad80456bad32363d99
Michael Ripley <zkxs00@gmail.com>
2017-06-07 20:09:42 -0500

    Remove all the mods

--- ds.sh
+++ ds.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 umask 002
-./ark/ShooterGame/Binaries/Linux/ShooterGameServer TheCenter?listen?SessionName=DannoDanDan?MaxPlayers=30?BanListURL="http://playark.com/banlist.txt"?AlwaysNotifyPlayerJoined=True?AlwaysNotifyPlayerJoin=True -NoBattlEye -nosteamclient -server -log -automanagedmods
+./ark/ShooterGame/Binaries/Linux/ShooterGameServer TheCenter?listen?SessionName=DannoDanDan?MaxPlayers=30?BanListURL="http://playark.com/banlist.txt"?AlwaysNotifyPlayerJoined=True?AlwaysNotifyPlayerJoin=True -NoBattlEye -nosteamclient -server -log
